10cadb6cd4e88a6b94196624ebf6cb23d32d141b
Vansh <kharevansh13@gmail.com>
2023-08-19 13:30:17 +0530

    Initial commit for changing code in next

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'lightbg':'#D9E3F2',
+        'bluegray':'#4F6483',
+        'bluel':'#375F9B',
+        'bluedark':'#1B2E4B',
+      }
     },
   },
   plugins: [],
